2004c886b4f4f2206b00a20681ccfd5450d938c1
maruf-229 <79322030+maruf-229@users.noreply.github.com>
2021-02-23 11:55:22 +0600

    update-4

--- routes/web.php
+++ routes/web.php
@@ -30,9 +30,9 @@ Route::group(['prefix'=>'admin', 'as' => 'admin.', 'middleware' => ['auth']], fu
 
         Route::get('/', [CategoryController::class,'index'])->name('backend.category');
         Route::get('/create', [CategoryController::class, 'create'])->name('backend.category.create');
-        Route::get('/edit/{id}', [CategoryController::class,])->name('backend.category.edit');
+        Route::get('/edit/{id}', [CategoryController::class,'edit'])->name('backend.category.edit');
         Route::post('/store', [CategoryController::class, 'store'])->name('backend.category.store');
-        Route::post('/edit/{id}', [CategoryController::class,])->name('backend.category.update');
+        Route::post('/edit/{id}', [CategoryController::class,'update'])->name('backend.category.update');
         //Route::post('/delete/{id}', [CategoryController::class,])->name('admin.categories.delete');
     });
 
